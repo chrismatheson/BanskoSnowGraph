9dbe785b6bb26ffee9269c758ccb4315fa674863
Chris Matheson <chris@matheson.it>
2013-01-18 08:31:17 -0500

    basi scrapper now working

---
+++ App.js
@@ -0,0 +1,30 @@
+var scraper = require("scraper"),
+    Parse = require("parse-api").Parse;
+    
+var db = new Parse("YGVEqwNJuHn8ujqO6Wzg1fm8WQARJzAs0ASxMri1","cXn3RHJpm2DeTPfTcGI2tYQKwI83bsPmJHHLerU2");
+
+function parseResponse (err, data) {
+  if (err) {
+    console.warn(err); 
+  }
+}
+
+setInterval(function(){
+  scraper("http://www.banskoski.com/en/", function(err, $) {
+    if (err) {console.log(err);}
+  
+    var rows = $('.meteo-details-tbl').find('tr:gt(0)');
+    
+    for (var i = 0; i < rows.length; i++) {
+      var result = {};
+      result.area = $(rows[i]).children('td:eq(0)').html();
+      result.temp = parseFloat($(rows[i]).children('td:eq(1)').html());
+      result.windSpeed = parseFloat($(rows[i]).children('td:eq(2)').html());
+      result.snowDepth = parseFloat($(rows[i]).children('td:eq(3)').html());
+      
+      console.log(result.area +" : "+JSON.stringify(result));
+      db.insert('res', result, parseResponse);
+    }
+  }); 
+}, 60000);
+
